1a3ca17e39b23394520fb07a6a7bc37a52b35ea8
Forvi <nforvi@mail.ru>
2025-08-29 04:28:30 +0500

    - new product folder window - fix back button

--- src/main/java/org/example/myskladreport/controllers/ProductFolderController.java
+++ src/main/java/org/example/myskladreport/controllers/ProductFolderController.java
@@ -1,23 +1,8 @@
 package org.example.myskladreport.controllers;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.List;
-import java.util.Objects;
-import java.util.ResourceBundle;
-import org.controlsfx.control.CheckListView;
-import org.controlsfx.control.PopOver;
-import org.example.myskladreport.HelloApplication;
-import org.example.myskladreport.models.ProductFolder;
-import org.example.myskladreport.models.RetailStore;
-import org.example.myskladreport.utils.FolderChooser;
-import org.example.myskladreport.utils.ReportWriter;
-import org.example.myskladreport.utils.SkladRequest;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -30,17 +15,39 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import org.controlsfx.control.ListSelectionView;
+import org.controlsfx.control.PopOver;
+import org.example.myskladreport.HelloApplication;
+import org.example.myskladreport.models.ProductFolder;
+import org.example.myskladreport.models.RetailStore;
+import org.example.myskladreport.utils.FolderChooser;
+import org.example.myskladreport.utils.ReportWriter;
+import org.example.myskladreport.utils.SkladRequest;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ProductFolderController implements Initializable {
 
     @FXML
-    private TextField listSearch;
+    private Button exitButton;
 
     @FXML
-    private CheckListView<ProductFolder> checkListView;
+    private TextField listSearchAvailable;
+
+    @FXML
+    private TextField listSearchSelected;
 
     @FXML
     private Button nextButton;
+
+    @FXML
+    private ListSelectionView<ProductFolder> listSelectionView;
 
     @FXML
     private Button questionButton;
@@ -51,41 +58,65 @@ public class ProductFolderController implements Initializable {
     @FXML
     private Button backButton;
 
-    @FXML
-    private Button clearAllButton;
+    List<ProductFolder> productFolders;
 
-    @FXML
-    private Button selectAllButton;
+    private Stage stage;
 
-    private ObservableList<RetailStore> retailStores;
+    private final ObservableList<ProductFolder> availableProductFolders = FXCollections.observableArrayList();
 
-    private final String URL = "https://api.moysklad.ru/api/remap/1.2/entity/productfolder/";
+    private final ObservableList<ProductFolder> selectedProductFolders = FXCollections.observableArrayList();
 
-    ObservableList<ProductFolder> masterData;
+    private ObservableList<RetailStore> retailStoresSelected;
 
-    private FilteredList<ProductFolder> filteredData;
+    private ObservableList<RetailStore> retailStoresAll;
 
     private SkladRequest skladRequest;
 
-    private Stage stage;
-    
+    private final String URL = "https://api.moysklad.ru/api/remap/1.2/entity/productfolder/";
+
     // ======== INIT =============
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        masterData = FXCollections.observableArrayList();
-        filteredData = new FilteredList<>(masterData, p -> true);
-        checkListView.setItems(filteredData);
+        listSelectionView.setSourceHeader(new Label(""));
+        listSelectionView.setTargetHeader(new Label(""));
 
-        searchHandler();
-        lookSelectedHandler();
+        listSelectionView.setSourceItems(availableProductFolders);
+        listSelectionView.setTargetItems(selectedProductFolders);
+
+        setupSearchAvailable();
+        setupSearchSelected();
+    }
+    
+    private void loadData() {
+        try {
+            var responseGzip = this.skladRequest.sendGetRequest(URL);
+            var response = this.skladRequest.unpackedGzip(responseGzip);
+            
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode node = objectMapper.readTree(response);
+            
+            productFolders = this.skladRequest.getProductFoldersFromSklad(node);
+            availableProductFolders.setAll(productFolders);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     // ======== SETTERS =============
-    public void setRetailStores(ObservableList<RetailStore> retailStores) {
-        if (retailStores.isEmpty() || Objects.isNull(retailStores))
+
+    public void setSelectedRetailStores(ObservableList<RetailStore> retailStoresSelected) {
+        if (retailStoresSelected.isEmpty() || Objects.isNull(retailStoresSelected))
             throw new IllegalArgumentException("Retail Stores cannot be empty or null.");
 
-        this.retailStores = retailStores;
+        this.retailStoresSelected = retailStoresSelected;
+    }
+
+    public void setAvailableRetailStores(ObservableList<RetailStore> retailStoresAvailable) {
+        if (retailStoresAvailable.isEmpty() || Objects.isNull(retailStoresAvailable))
+            throw new IllegalArgumentException("Retail Stores cannot be empty or null.");
+
+        this.retailStoresAll = retailStoresAvailable;
     }
 
     public void setToken(String token) {
@@ -99,71 +130,40 @@ public class ProductFolderController implements Initializable {
     }
 
     // ======== HANDLERS =============
-    private void loadData() {
-        try {
-            var responseGzip = this.skladRequest.sendGetRequest(URL);
-            var response = this.skladRequest.unpackedGzip(responseGzip);
-            
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode node = objectMapper.readTree(response);
-            
-            List<ProductFolder> productFolders = this.skladRequest.getProductFoldersFromSklad(node);
-            masterData.clear();
-            masterData.addAll(productFolders);
-            
-            selectButton.setOnAction(e -> {
-                lookSelectedHandler();
-            });
 
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
+    /**
+     * <p>Обработчик кнопки "Выйти".</p>
+     * <p>Загружает окно авторизации.</p>
+     * 
+     * @param event
+     * @throws IOException
+     */
     @FXML
-    protected void nextButtonHandler() {
-        ObservableList<ProductFolder> productFolders = checkListView.getCheckModel().getCheckedItems();
+    protected void exitButtonHandler(ActionEvent event) throws IOException {
+        Stage currentStage = (Stage) exitButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-password.fxml"));
+        Parent root = fxmlLoader.load();
         
-        try {
-            String path = FolderChooser.choose(stage, "Выберите папку");
-            ReportWriter.write(this.retailStores, productFolders, skladRequest, path);
-            successfulSaveModal(stage);
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
+        Stage newStage = new Stage();
+        newStage.setTitle("MySklad Report App");
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(HelloApplication.class.getResource("styles/styles.css").toExternalForm());
+        newStage.setScene(scene);
+        newStage.setResizable(false);
+        newStage.centerOnScreen();
+        currentStage.close();
         
+        newStage.show();
     }
 
-    private void lookSelectedHandler() {
-
-        selectButton.setOnAction(e -> {
-            ObservableList<ProductFolder> selected = checkListView.getCheckModel().getCheckedItems();
-            StringBuilder sb = new StringBuilder();
-
-            for (var store : selected) {
-                sb.append(store.getName()).append("\n");
-            }
-
-            Label content = new Label();
-            if (sb.isEmpty()) {
-                content.setText("Вы ничего не выбрали!");;
-            } else {
-                content.setText(sb.toString());
-            }
-
-            content.setWrapText(true);
-            VBox vbox = new VBox(content);
-            vbox.setPadding(new Insets(12));
-
-            PopOver popOver = new PopOver(vbox);
-            popOver.setArrowLocation(PopOver.ArrowLocation.TOP_CENTER);
-            popOver.show(selectButton);
-        });
-    }
-
+    /**
+     * <p>Обработчик кнопки "?"</p>
+     * 
+     * @param event
+     */
     @FXML
-    protected void questionButtonHandler() {
-        Label text = new Label("- Выберите группы товаров, для которых Вы хотите просмотреть и выгрузить информацию.\n" + 
+    protected void questionButtonHandler(ActionEvent event) {
+        Label text = new Label("- Выберите точки продаж, для которых Вы хотите просмотреть и выгрузить информацию.\n" + 
                                 "- Для быстрого поиска введите полное или частичное название в текстовое поле.");
         VBox vbox = new VBox(text);
         vbox.setPadding(new Insets(15));
@@ -174,14 +174,96 @@ public class ProductFolderController implements Initializable {
         });
     }
 
-    private void searchHandler() {
-        filteredData = new FilteredList<>(masterData, p -> true);
-        listSearch.setOnAction(event -> {
-            String newVal = listSearch.getText();
-            filteredData.setPredicate(store -> store.getName().toLowerCase().contains(newVal.toLowerCase()));
-        });
+    @FXML
+    protected void nextButtonHandler() {
+        try {
+            String path = FolderChooser.choose(stage, "Выберите папку");
+            ReportWriter.write(this.retailStoresSelected, selectedProductFolders, skladRequest, path);
+            successfulSaveModal(stage);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        
+    }
 
-        checkListView.setItems(filteredData);
+    @FXML
+    private void backButtonHandler() throws IOException {
+        Stage stage = (Stage) backButton.getScene().getWindow();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("retail-store.fxml"));
+        Parent root = fxmlLoader.load();
+        RetailStoreController retailStoreController = fxmlLoader.getController();
+        retailStoreController.setListSearchSelected(retailStoresSelected);
+        retailStoreController.setListSearchAvailable(retailStoresAll);
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(HelloApplication.class.getResource("styles/styles.css").toExternalForm());
+        stage.setScene(scene);
+        stage.setTitle("Точки продаж");
+    }
+
+    /**
+     * <p>Производит поиск в списке доступных элементов.</p>
+     */
+    private void setupSearchAvailable() {
+        listSearchAvailable.textProperty().addListener((obs, oldVal, newVal) -> {
+            filterAvailableList(newVal);
+        });
+    }
+
+    /**
+     * <p>Производит поиск в списке выбранных элементов.</p>
+     */
+    private void setupSearchSelected() {
+        listSearchSelected.textProperty().addListener((obs, oldVal, newVal) -> {
+            filterSelectedList(newVal);
+        });
+    }
+
+    /**
+     * <p>Фильтрует список доступных элементов по подстроке.</p>
+     * 
+     * @param filter подстрока
+     */
+    private void filterAvailableList(String filter) {
+        availableProductFolders.clear();
+        String lowerFilter = filter == null ? "" : filter.toLowerCase();
+        List<ProductFolder> filtered = productFolders.stream()
+            .filter(store -> store.getName().toLowerCase().contains(lowerFilter))
+            .filter(store -> !selectedProductFolders.contains(store))
+            .collect(Collectors.toList());
+
+        availableProductFolders.addAll(filtered);
+    }
+
+    /**
+     * <p>Фильтрует список выбранных элементов по подстроке.</p>
+     * 
+     * @param filter подстрока
+     */
+    private void filterSelectedList(String filter) {
+        selectedProductFolders.clear();
+        String lowerFilter = filter == null ? "" : filter.toLowerCase();
+        List<ProductFolder> filtered = productFolders.stream()
+            .filter(store -> store.getName().toLowerCase().contains(lowerFilter))
+            .filter(store -> !availableProductFolders.contains(store))
+            .collect(Collectors.toList());
+
+        selectedProductFolders.addAll(filtered);
+    }
+
+    /**
+     * <p>Показывает отсутсвие выбранных точек продаж.</p>
+     */
+    private void showEmptySelectedHandler() {
+        Label content = new Label("Вы ничего не выбрали!");
+
+        content.setWrapText(true);
+        VBox vbox = new VBox(content);
+        vbox.setPadding(new Insets(12));
+
+        PopOver popOver = new PopOver(vbox);
+        popOver.setArrowLocation(PopOver.ArrowLocation.TOP_CENTER);
+        popOver.show(nextButton);
     }
 
     public void successfulSaveModal(Stage stage) {
@@ -200,37 +282,4 @@ public class ProductFolderController implements Initializable {
             e.printStackTrace();
         }
     }
-
-    @FXML
-    private void backButtonHandler() throws IOException {
-        Stage stage = (Stage) backButton.getScene().getWindow();
-
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("retail-store.fxml"));
-        Parent root = fxmlLoader.load();
-
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add(HelloApplication.class.getResource("styles/styles.css").toExternalForm());
-        stage.setScene(scene);
-        stage.setTitle("Точки продаж");
-    }
-
-    @FXML
-    protected void clearAllButtonHandler() {
-        try {
-            checkListView.getCheckModel().clearChecks();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @FXML
-    protected void selectAllButtonHandler() {
-        try {
-            checkListView.getCheckModel().checkAll();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
-
-
